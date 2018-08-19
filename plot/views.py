8d2704029ba86defaf5ccf2c77d7a00cb6adc36a
Felix <alexei.eremin@gmail.com>
2018-08-19 10:37:27 +0300

    working on plot view

--- plot/views.py
+++ plot/views.py
@@ -4,80 +4,56 @@ import matplotlib as mpl
 mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn
-from sklearn.datasets import make_classification
+
+from django.views.generic import TemplateView
 
 
 
-def main():
-
-    print('start')
-    features = np.load('features.npy')
-    person_id = np.load('person_id.npy')
+class PlotViev(TemplateView):
+    template_name = "plot.html"
 
 
-    #feature_similarity, true_labels = get_similarity(features, features[0], person_id[0], person_id)
-    feature_similarity, true_labels = get_total_similarity(features, person_id)
+    def post(self, request, *args, **kwargs):
+        context = {}
+        features = request.FILES['features']
+        person_id = request.FILES['person_id']
 
 
-    # actual code for roc + threshold charts start here
-    # compute fpr, tpr, thresholds and roc_auc
-    print(len(feature_similarity), len(true_labels))
-    print(feature_similarity, true_labels)
-    fpr, tpr, thresholds = roc_curve(true_labels, feature_similarity)
-    print(len(tpr), tpr)
-    print(len(fpr), fpr)
-    print(len(thresholds), thresholds)
-    roc_auc = auc(fpr, tpr)  # compute area under the curve
-
-    plt.figure()
-    plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % (roc_auc))
-    plt.plot([0, 1], [0, 1], 'k--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver operating characteristic')
-    plt.legend(loc="lower right")
-
-    # create the axis of thresholds (scores)
-    ax2 = plt.gca().twinx()
-    ax2.plot(fpr, thresholds, markeredgecolor='r', linestyle='dashed', color='r')
-    ax2.set_ylabel('Threshold', color='r')
-    ax2.set_ylim([thresholds[-1], thresholds[0]])
-    ax2.set_xlim([fpr[0], fpr[-1]])
-
-    plt.savefig('roc_and_threshold_3.png')
-    plt.close()
+        #feature_similarity, true_labels = get_similarity(features, features[0], person_id[0], person_id)
+        feature_similarity, true_labels = get_total_similarity(features, person_id)
 
 
-def get_similarity(features, current_vector, current_person, person_id):
-    list_similarity = []
-    list_classify = []
-    confidence_values = []
+        # actual code for roc + threshold charts start here
+        # compute fpr, tpr, thresholds and roc_auc
+        print(len(feature_similarity), len(true_labels))
+        print(feature_similarity, true_labels)
+        fpr, tpr, thresholds = roc_curve(true_labels, feature_similarity)
+        print(len(tpr), tpr)
+        print(len(fpr), fpr)
+        print(len(thresholds), thresholds)
+        roc_auc = auc(fpr, tpr)  # compute area under the curve
 
-    T = 0
+        plt.figure()
+        plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % (roc_auc))
+        plt.plot([0, 1], [0, 1], 'k--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver operating characteristic')
+        plt.legend(loc="lower right")
 
-    for index, vector in enumerate(features):
-        similarity = np.dot(vector, current_vector)
-        list_similarity.append(similarity)
-        person = person_id[index]
-        if similarity >= T:
-            if not person == current_person:
-                T = similarity
+        # create the axis of thresholds (scores)
+        ax2 = plt.gca().twinx()
+        ax2.plot(fpr, thresholds, markeredgecolor='r', linestyle='dashed', color='r')
+        ax2.set_ylabel('Threshold', color='r')
+        ax2.set_ylim([thresholds[-1], thresholds[0]])
+        ax2.set_xlim([fpr[0], fpr[-1]])
 
-    for sim in list_similarity:
-        confidence = sim/T
-        #print('similarity: {0} | T: {1} | Probabilty: {2}'.format(sim, T, confidence))
-        confidence_values.append(confidence)
-        if sim >= T:
-            list_classify.append(1)
-        else:
-            list_classify.append(0)
+        plt.savefig('roc_and_threshold_3.png')
+        plt.close()
 
-    feature_similarity = np.array(list_similarity)
-    true_labels = np.array(list_classify)
-    return feature_similarity, true_labels
+        return render(request, 'plot.html', {'context': context})
 
 
 def get_total_similarity(features, person_id):
@@ -111,6 +87,3 @@ def get_total_similarity(features, person_id):
     feature_similarity = np.array(list_similarity)
     true_labels = np.array(list_classify)
     return feature_similarity, true_labels
-
-
-main()
